0ca258301d506640751397746474b110f206572b
Anderson Braz <braz_anderson@msn.com>
2020-05-18 10:40:32 -0300

    Up Files

--- index.js
+++ index.js
@@ -4,6 +4,18 @@ const morgan = require('morgan');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 
+// importando pacote do Prometheus
+const client = require('prom-client');
+const collectDefaultMetrics = client.collectDefaultMetrics;
+collectDefaultMetrics({ prefix: 'my_api_nodejs:' });
+
+const histogram = new client.Histogram({
+    name: 'my_api_nodejs:sample_duration',
+    help: 'Duration of HTTP requests in ms',
+    labelNames: ['method', 'status_code'],
+    buckets: [0.1, 5, 15, 50, 100, 500]
+  });
+
 // crio um servidor express
 const app = express();
 
@@ -23,29 +35,40 @@ const members = ["Anderson", "Caio", "Diogo", "Jeovah", "Jair", "Banco do Barsil
 
 // criação de rota que será acessada utilizando o método HTTP GET/
 app.get('/', (req, res) => {
+    const end = histogram.startTimer();
+    end({ method: req.method, 'status_code': 200 });
     return res.json({ data });
 });
 
 app.get('/alphabet', (req, res) => {
+    const end = histogram.startTimer();
+    end({ method: req.method, 'status_code': 200 });
     return res.json({ alphabet });
 });
 
 app.get('/members', (req, res) => {
+    const end = histogram.startTimer();
+    end({ method: req.method, 'status_code': 200 });
     return res.json({ members });
 });
 
-// criação de rota que será acessada utilizando o método HTTP POST/
-// http://localhost:9000/add
 app.post('/add', (req, res) => {
     const result = req.body;
+    const end = histogram.startTimer();
 
     if (!result) {
         return res.status(400).end();
     }
 
     data.push(result);
+    end({ method: req.method, 'status_code': 200 });
     return res.json({ result });
 
+});
+
+app.get('/metrics', (req, res) => {
+    res.set('Content-Type', client.register.contentType);
+    res.send(client.register.metrics());
 });
 
 // o servidor irá rodar dentro da porta 9000
