420c56e7917ec6d8307fdc14c2c5356eb0c53d60
Anderson Braz <braz_anderson@msn.com>
2020-05-19 10:40:52 -0300

    Up Files

--- index.js
+++ index.js
@@ -30,14 +30,32 @@ const gauge = new client.Gauge({
 });
 
 const counter = new client.Counter({
-    name: 'sample_counter', 
+    name: 'sample_counter',
     help: 'The number of requests'
 });
 
-counter.inc();
+var myMetrics = function (req, res, next) {
+
+    const eh = histogram.startTimer();
+    const es = summary.startTimer();
+    const eg = gauge.startTimer();
+
+    eh({ method: req.method, 'status_code': 200 });
+    es({ method: req.method, 'status_code': 200 });
+    gauge.set({ method: req.method, 'status_code': '200' }, 100);
+    gauge.labels(req.method, '200').set(100);
+    counter.inc();
+
+    console.log("my middleware");
+    next();
+
+}
 
 // crio um servidor express
 const app = express();
+
+// aplicando um Middleware
+app.use(myMetrics);
 
 // aplico configurações para dentro do servidor express, adicionando middlewares (body-parser, morgan, cors)
 app.use(morgan('dev'));
@@ -53,26 +71,14 @@ const data = [{ title: "Node API Express", version: "4.17.1" }];
 const alphabet = [{ "id": 1, "caption": "alpha" }, { "id": 2, "caption": "bravo" }, { "id": 3, "caption": "charlie" }, { "id": 4, "caption": "delta" }, { "id": 5, "caption": "eco" }, { "id": 6, "caption": "fox" }, { "id": 7, "caption": "golf" }, { "id": 8, "caption": "hotel" }, { "id": 9, "caption": "india" }, { "id": 10, "caption": "juliet" }, { "id": 11, "caption": "kilo" }, { "id": 12, "caption": "lima" }, { "id": 13, "caption": "mike" }, { "id": 14, "caption": "november" }, { "id": 15, "caption": "oscar" }, { "id": 16, "caption": "papa" }, { "id": 17, "caption": "quebec" }, { "id": 18, "caption": "romeu" }, { "id": 19, "caption": "sierra" }, { "id": 20, "caption": "tango" }, { "id": 21, "caption": "uniform" }, { "id": 22, "caption": "victor" }, { "id": 23, "whisky": "mike" }, { "id": 24, "caption": "x-ray" }, { "id": 25, "caption": "yankee" }, { "id": 26, "caption": "zulu" }];
 const members = ["Anderson", "Caio", "Diogo", "Jeovah", "Jair", "Banco do Brasil", "Capgemini"];
 
-
 // criação de rota que será acessada utilizando o método HTTP GET/
 app.get('/', (req, res) => {
 
-    const eh = histogram.startTimer();
-    eh({ method: req.method, 'status_code': 200 });
     return res.json({ data });
 
 });
 
 app.get('/alphabet', (req, res) => {
-    
-    const eh = histogram.startTimer();
-    const es = summary.startTimer();
-    const eg = gauge.startTimer();
-
-    eh({ method: req.method, 'status_code': 200 });
-    es({ method: req.method, 'status_code': 200 });
-    gauge.set({ method: req.method, 'status_code': '200' }, 100);
-    gauge.labels(req.method, '200').set(100);
 
     return res.json({ alphabet });
 
@@ -80,27 +86,18 @@ app.get('/alphabet', (req, res) => {
 
 app.get('/members', (req, res) => {
 
-    const eh = histogram.startTimer();
-    const es = summary.startTimer();
-
-    eh({ method: req.method, 'status_code': 200 });
-    es({ method: req.method, 'status_code': 200 });
     return res.json({ members });
 
 });
 
 app.post('/add', (req, res) => {
     const result = req.body;
-    const eh = histogram.startTimer();
-    const es = summary.startTimer();
 
     if (!result) {
         return res.status(400).end();
     }
 
     data.push(result);
-    eh({ method: req.method, 'status_code': 200 });
-    es({ method: req.method, 'status_code': 200 });
     return res.json({ result });
 
 });
